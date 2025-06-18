a526514f82248842b59fd37014a929a7c002b463
zaynab <hadi60787@gmail.com>
2025-06-18 15:19:46 +0300

    create list to store student names and grades

--- students-grade-analyzer.py
+++ students-grade-analyzer.py
@@ -1,2 +1,6 @@
 #create integer input field 
 num_of_students=int(input("Hello teacher ! enter the number of your students\n"))
+#create list to store student names and grades
+student_name=[]
+student_grade=[]
+
